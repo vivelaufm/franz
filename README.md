2260c4d6445336e7f64a033f0eb75b6601165218
Marcos Cano <>
2017-08-09 02:23:35 -0600

    Genesis 1:2

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# franz
+# Franz
+
+Vive La UFM's Lex bot
+
+
+# 1. Login to AWS
+
+https://ufm-edu.signin.aws.amazon.com/console
+
+`user:` 
+
+`password:` located at your desktop
+
+# 2. Create YOUR Lambda Function
+
+Services >> Lambda >> Create Function >> Author from scratch >> Next >> 
+
+- `Name:` VivaLaUFM_Initials
+- `Description:` Feel free to put whatever
+- `Runtime`: Node.js 4.3
+- `Lambda Function Code:` copy paste `main.js`
+- `Lambda function handler and role`: Choosee an Existing Roles -> ViveLaUFM
+
+
+
+# 3. Create YOUR Lex bot
+
+Services >> Lex >> Create >> Custom Bot
+
+- `Bot name:` VivaLaUFM_Initials
+- `Output voice:` Justin
+- `Session Timeout:` 5 min
---
+++ main.js
@@ -0,0 +1,102 @@
+'use strict';
+ 
+
+
+var BotName="Franz"
+
+function getMessage (type) {
+
+     var message = {
+        'Despacito': function () {
+          return "Comin over in my direction Dess pa cito . Quiero respirar tu cuello despacito. \
+            Deja que te diga cosas al oído. Pasito a pasito, suave suavecito Nos vamos pegando, \
+            poquito a poquito";
+           
+        },
+        'AboutYourself': function () {
+            let birthday = new Date();
+            birthday= `${birthday}`.substring(0,`${birthday}`.indexOf("GMT"));
+            return `Hello my name is ${BotName}. I'm your personal assistant. \
+            I was born on ${birthday} at,  be ve la UFM  \
+            which is an event by Universidad Francisco Marroquin `;
+                
+        },
+
+ 
+        'default': function () {
+          return 'you know I love you <3' ;
+        }
+      };
+  
+
+
+  return (message[type]() || message['default']());
+}
+
+
+
+function getDrink (type) {
+  var drinks = {
+    'coke': 'Coke',
+    'pepsi': 'Pepsi',
+    'lemonade': 'Lemonade',
+    'default': 'Default item'
+  };
+  return 'The drink I chose was ' + (drinks[type] || drinks['default']);
+}
+
+
+//DO NOT TOUCH CODE BELOW
+//----------------------------------------------------
+
+
+// Close dialog with the customer, reporting fulfillmentState of Failed or Fulfilled ("Thanks, your pizza will arrive in 20 minutes")
+function close(sessionAttributes, fulfillmentState, message) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'Close',
+            fulfillmentState,
+            message,
+        },
+    };
+}
+ 
+// --------------- Events -----------------------
+ 
+
+function dispatch(intentRequest, callback) {
+    console.log('request received for userId=${intentRequest.userId}, intentName=${intentRequest.currentIntent.intentName}');
+    const sessionAttributes = intentRequest.sessionAttributes;
+    const slots = intentRequest.currentIntent.slots;
+    console.log(intentRequest)
+    var intentName= intentRequest.currentIntent.name;
+    //console.log(`Requeste received for IntentName = ${intentName}`);
+    // var name = slots.name;
+    // var summary = slots.summary;
+    // console.log(`request received for Slots=${name}, ${summary}`);
+    
+    var msg = getMessage(intentName);
+    console.log("Sending the following message: " + msg)
+
+    callback(close(sessionAttributes, 'Fulfilled',
+    {'contentType': 'PlainText', 'content': msg}));
+
+
+}
+
+// --------------- Main handler -----------------------
+ 
+// Route the incoming request based on intent.
+// The JSON body of the request is provided in the event slot.
+exports.handler = (event, context, callback) => {
+    try {
+        dispatch(event,
+            (response) => {
+                callback(null, response);
+            });
+    } catch (err) {
+        callback(err);
+    }
+};
+
